9fb0325f3155a8dcd0470754a3b200fa365887a2
Abd El Djalil Saf <131856785+AbdEDjalilSaf@users.noreply.github.com>
2023-10-17 06:52:23 +0100

    e-commerce slides images

--- src/Collection.jsx
+++ src/Collection.jsx
@@ -6,25 +6,62 @@ function Collection() {
 
 // let appendZero = (value) => (value < 10 ? "0" + value : value);
 
-let [time,setTime] = useState(new Date());
+// let [time,setTime] = useState(new Date());
 
-useEffect(()=>{
+// useEffect(()=>{
 
-let interValid = setInterval(()=>{
-     setTime(new Date());
-},1000);
+// let interValid = setInterval(()=>{
+//      setTime(new Date());
+// },1000);
 
-return ()=>{
-    clearInterval(interValid);
-}
+// return ()=>{
+//     clearInterval(interValid);
+// }
 
-},[]);
-
-// let day = appendZero(time.getDay());
-// let hour = appendZero(time.getHours());
-// let minutes = appendZero(time.getMinutes());
-// let seconds = appendZero(time.getSeconds());
-
+// },[]);
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "09/30/",
+        birthday = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+      birthday = dayMonth + nextYear;
+    }
+    //end
+    
+    const countDown = new Date(birthday).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "It's my birthday!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+    }());
 
   return (
     <div className='collect py-28 flex flex-col gap-3'>
@@ -45,16 +82,15 @@ return ()=>{
 <h4 className="text-red-500 text-left  font-semibold">DEAL OF THE WEEK</h4>
 <h2 className='text-4xl font-semibold'>Multi-pocket Chest Bag Black</h2>
 <div>
-    {/* <div className='timeNumber  flex'>
-<span className='daysNumber flex flex-col items-center gap-2'>{day}<h4 className='String '>Days</h4></span>
-<span>:</span>
-<span className='hourNumber flex flex-col items-center gap-2'>{hour}<h4 className='String'>Hours</h4></span>
-<span>:</span>
-<span className='minuteNumber flex flex-col items-center gap-2'>{minutes}<h4 className='String'>Minutes</h4></span>
-<span>:</span>
-<span className='secondNumber flex flex-col items-center gap-2' >{seconds}<h4 className='String'>Seconds</h4></span>
-    </div> */}
-    <iframe  src="https://w2.countingdownto.com/5048537" frameborder="0" className='fitwidth'></iframe>
+    {/* <iframe  src="https://w2.countingdownto.com/5048537" frameborder="0" className='fitwidth'></iframe> */}
+    <div id="countdown">
+    <ul>
+      <li><span id="days"></span>days</li>
+      <li><span id="hours"></span>Hours</li>
+      <li><span id="minutes"></span>Minutes</li>
+      <li><span id="seconds"></span>Seconds</li>
+    </ul>
+  </div>
 </div>
 <button className="bg-black text-white w-fit textbutton font-semibold py-4 px-11"> S H O P <span className="ml-3">N O W </span></button>
 </div>
